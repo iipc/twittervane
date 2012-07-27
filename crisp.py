82be107d54700315f8f1281a8129802ee57f830c
Andrew Jackson <Andrew.Jackson@bl.uk>
2012-07-27 07:26:07 +0100

    Updated to modify a different sheet.

--- crisp.py
+++ crisp.py
@@ -6,6 +6,12 @@ import urlparse
 import urllib2
 import ConfigParser
 import simplejson
+import sys
+from datetime import datetime
+
+# Force unicode behaviour:
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def unshorten_url(url):
   try:
@@ -24,17 +30,22 @@ def unshorten_url(url):
   except:
 	return url
 
+max_id = "226230572954038272"
+
 # fetch the url
-url = "http://search.twitter.com/search.json?q=%40dpref&rpp=100"
+url = "http://search.twitter.com/search.json?q=%40dpref&rpp=100i&since_id={}".format(max_id)
 json = urllib2.urlopen(url).read()
-tweets = simplejson.loads(json)
-for tweet in tweets['results']:
+tweets = simplejson.loads(json, encoding = 'utf-8')
+for tweet in reversed(tweets['results']):
     tags = re.findall(r'#(\S+)', tweet["text"])
     urls = re.findall(r'(https?://\S+)', tweet["text"])
+    #Fri, 20 Jul 2012 08:17:09 +0000
+    d = datetime.strptime( tweet['created_at'], '%a, %d %b %Y %H:%M:%S +0000')
+    date = d.strftime('%d/%m/%Y %H:%M:%S')
     if len(urls) == 0:
         urls = [""]
     for url in urls:
-        print "\"{}\", \"{}\", \"{}\", \"{}\", \"@{}\"".format( tweet['created_at'], unshorten_url(url), ','.join(tags), tweet['text'], tweet['from_user'])
+        print "\"{}\", \"{}\", \"{}\", \"{}\", \"@{}\", \"{}\"".format( date, unshorten_url(url), ','.join(tags), tweet['text'], tweet['from_user'], tweet['id'])
 
 def tweet_stream():
     config = ConfigParser.ConfigParser()
--- crisp_spread.py
+++ crisp_spread.py
@@ -10,8 +10,10 @@ gpw = config.get("google", "pw")
 # Login with your Google account
 gc = gspread.login(guser, gpw)
 
-# Open a worksheet from spreadsheet with one shot
-wks = gc.open("CRISP Data Collection Form").sheet1
+# Open a worksheet from spreadsheet
+ss = gc.open("CRISP Data Collection Form")
+#wks = ss.sheet1 # Submissions sheet
+wks = ss.get_worksheet(1) # Tweets sheet
 
 # Get all values from column. Column and row indexes start from one
 first_col = wks.col_values(2)
@@ -22,5 +24,5 @@ for url in first_col:
     print url
 
 
-#wks.update_cell(erow, 2, 'http://test/')
+wks.update_cell(erow, 2, 'http://test/')
 
