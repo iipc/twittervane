b887d2c47b5a7d8733fae11ad7805fbb0e3bff40
Andrew Jackson <Andrew.Jackson@bl.uk>
2012-08-02 13:41:13 +0100

    Auto-update from cron.

--- vane_cron.sh
+++ vane_cron.sh
@@ -2,6 +2,7 @@
 cd /home/anjackson/git/crisp
 /usr/local/bin/python2.7 vane_ana.py
 /usr/local/bin/python2.7 vane_aggra.py
+git checkout gh-pages
 git add json/tweetlog.2012-*.json
 git commit -a -m "Auto-update from cron."
-git push
+git push origin gh-pages
